5c39abe8db4db83c0de06108e26808ba89c4081c
MahatmaK <nirajkhatri21@gmail.com>
2021-06-24 16:28:52 -0400

    Completed Bonus. Define switch iterate

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,34 +1,43 @@
 // POPULATE TEST SUBJECT ID #S
 // -------------------------------------------------------- //
 
-// Grab <select> tag to capture dropdown
-var dropdown = d3.select("select");
-
-// Define path to data file 'samples'
-var path = `samples.json`;
-
-// Pull data from json. 'Then' define variables
-d3.json(path).then((data) => {
+function init() {
     
-    // Pull array of ID numbers from dataset
-    var names = data.names;
+    // Grab <select> tag to capture dropdown
+    var dropdown = d3.select("select");
+
+    // Define path to data file 'samples'
+    var path = `samples.json`;
+
+    // Pull data from json. 'Then' define variables
+    d3.json(path).then((data) => {
         
-    // Iterate through names array
-    for (var i = 0; i < names.length; i++ ) {
+        // Pull array of ID numbers from dataset
+        var names = data.names;
+            
+        // Iterate through names array
+        for (var i = 0; i < names.length; i++ ) {
 
-        // Add <option> tag after select
-        var option = dropdown.append("option");
+            // Add <option> tag after select
+            var option = dropdown.append("option");
 
-        // Add list of test subjects ids one by one
-        option.text(names[i]);
-    }    
-    
-    // Store current test subject into variable
-    var theSubject = d3.select("select").node().value;
+            // Add list of test subjects ids one by one
+            option.text(names[i]);
+        }    
+        
+        // Store current test subject into variable
+        var theSubject = d3.select("select").node().value;
 
-    // Populate intial demographic info (ID #940)
-    optionChanged(theSubject);
-}); 
+        // Populate initial demographic info (ID #940)
+        optionChanged(theSubject);
+
+
+    }); 
+}
+
+// Run page initilization
+init();
+
 // -------------------------------------------------------- //
 
 
@@ -46,7 +55,7 @@ function optionChanged(theSubject) {
     allCharts(theSubject);
 
     // Show gauge
-    
+    // plotGauge(); 
 
 }
 // -------------------------------------------------------- //
@@ -59,20 +68,34 @@ function optionChanged(theSubject) {
 // -------------------------------------------------------- //
 
 function demographicInfo(theSubject) {
-    
-    // Remove any existing <p> tags
-    d3.selectAll("p").remove();
 
+    // Remove any existing <p> tags
+    d3.select("#sample-metadata").selectAll("p").remove();
+
+    // Grab <select> tag to capture dropdown
+    var dropdown = d3.select("select");
+
+    // Define path to data file 'samples'
+    var path = `samples.json`;
+    
     // Pull data from json. 'Then' define variables
-    d3.json(path).then((data) => {
+    test(path, theSubject);
+    
+    
+};
+// -------------------------------------------------------- //
+
+// TEST FUNCTION //
+
+function test(path, theSubject) {
+    return d3.json(path).then((data) => {
 
         // Pull data for every subject from data into an array
         var metadata = data.metadata;
 
         // Iterate through each subject
         metadata.forEach(subjects => {
-        
-            // Find where subject from data matches users subject selection
+
             if (subjects.id == theSubject) {
 
                 // Iterate through all keys and their respective values
@@ -101,19 +124,88 @@ function demographicInfo(theSubject) {
 
                     // Set ID
                     paragraph.attr('id', key);
+
+                    // Store wfreq value
+                    if (key == 'wfreq') {
+                        wFreq = value;                        
+                    }    
                 });
             }
+            
+            
         });
+        
+        // Call plotGauge function to plot the gauge chart
+        plotGauge(wFreq)  
     });
-};
-// -------------------------------------------------------- //
+}
 
 
 
-// SHOW BAR CHART
+
+// return d3.json(path).then((data) => {
+
+    //     // Pull data for every subject from data into an array
+    //     var metadata = data.metadata;
+
+    //     // Iterate through each subject
+    //     metadata.forEach(subjects => {
+
+    //         if (subjects.id == theSubject) {
+
+    //             // Iterate through all keys and their respective values
+    //             Object.entries(subjects).forEach(([key, value]) => {
+                    
+    //                 // Change key to uppercase
+    //                 var upperKey = key.toUpperCase();
+
+    //                 // Define sentence to be added
+    //                 var sentence = `${upperKey}: ${value}`
+
+    //                 // Select where to append keys and values
+    //                 var demographic = d3.select("#sample-metadata");
+
+    //                 // Append <p> tag
+    //                 var paragraph = demographic.append("p");
+
+    //                 // Define what <p> should say
+    //                 paragraph.text(sentence); 
+                    
+    //                 // Change style of <p>
+    //                 paragraph.style("font-weight", "bold");
+                    
+    //                 // Change font 
+    //                 paragraph.style('font-family', 'Seriff');
+
+    //                 // Set ID
+    //                 paragraph.attr('id', key);
+
+    //                 // Store wfreq value
+    //                 if (key == 'wfreq') {
+    //                     wFreq = value;                        
+    //                 }    
+    //             });
+    //         }
+            
+            
+    //     });  
+    //     console.log(wFreq);
+    //     return(wFreq);       
+    // }); 
+
+
+
+
+// SHOW BAR AND BUBBLE
 // -------------------------------------------------------- //
 
 function allCharts(theSubject) {
+    
+    // Grab <select> tag to capture dropdown
+    var dropdown = d3.select("select");
+
+    // Define path to data file 'samples'
+    var path = `samples.json`;    
 
     // Pull data from json. 'Then' define variables
     d3.json(path).then((data) => {
--- StarterCode/static/js/bonus.js
+++ StarterCode/static/js/bonus.js
@@ -1,12 +1,19 @@
-function plotGauge() {
+function plotGauge(wFreqValue) {
     
-    var wFreqLoc = d3.select('#wfreq');
-    var wFreqValue = wFreqLoc.text().slice(-1);
-
+    // var wFreqLoc = d3.selectAll('#wfreq');
+    // console.log(wFreqLoc);
+    // var wFreqValue = wFreqLoc.text;
+    // console.log(wFreqValue);
+    
     var trace1 = {
         domain: { x: [0, 9], y: [0, 9] }, 
         value: wFreqValue,
-        title: "Belly Button Washing Frequency",
+        title: {
+            text: "Scrubs per Week",
+            font: {
+                color: 'Grey'
+            },
+        },   
         type: "indicator",
         mode: "gauge+number",
         gauge: {
@@ -18,7 +25,19 @@ function plotGauge() {
                 line: { color: "purple", width: 10 },
                 thickness: 2,
                 value: wFreqValue
-              }
+            },
+            steps: [
+                { range: [0, 1], color: 'rgb(245,248,248)' },
+                { range: [1, 2], color: 'rgb(235,242,242)' },
+                { range: [2, 3], color: 'rgb(215,229,229)' },
+                { range: [3, 4], color: 'rgb(195,216,216)' },
+                { range: [4, 5], color: 'rgb(175,203,204)' },
+                { range: [5, 6], color: 'rgb(155,190,191)' },
+                { range: [6, 7], color: 'rgb(135,178,178)' },
+                { range: [7, 8], color: 'rgb(115,164,165)' },
+                { range: [8, 9], color: 'rgb(95,152,153)' },
+            ]
+            
         },
 
     };
@@ -26,10 +45,20 @@ function plotGauge() {
     var data = [trace1];
 
     var layout = {
-        title:"Belly Button Washing Frequency 2"
+        title: {
+            text: "<b>Belly Button Washing Frequency 2<b>",
+            font: {
+                size: 30,
+            },
+        },
+        width: 600,
+        height: 450,
+        margin: {
+            b: 100,
+        }
     };
 
     Plotly.newPlot('gauge', data, layout);
 
-    console.log(wFreqValue);
+    
 };
